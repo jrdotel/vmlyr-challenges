898ae089e393189f3b5e5f4876d01599e64d0dff
Julio Dotel <julio.dotel@vmlyr.com>
2022-06-17 21:53:27 -0500

    project complete

--- challenge-5/script/script.js
+++ challenge-5/script/script.js
@@ -1,13 +1,38 @@
-const APIUrl = 'https://api.adviceslip.com/advice';
+const url = 'https://api.adviceslip.com/advice';
 
 let randomQuoteNum = document.querySelector('.advice__number');
 let randomQuote = document.querySelector('.advice__quote');
+let button = document.querySelector('.accent__button');
 
-fetch(APIUrl)
+// Add id and quote to html.
+function advice(id, quote) {
+  randomQuoteNum.innerHTML = `ADVICE #${id}`;
+  randomQuote.innerHTML = `"${quote}"`;
+}
+
+// Initial fetch for API.
+fetch(url)
   .then(response => response.json())
   .then((data) => {
-      console.log(data);
-      randomQuoteNum.innerHTML = `ADVICE #${data.slip.id}`;
-      randomQuote.innerHTML = `"${data.slip.advice}"`;
+      let id = data.slip.id
+      let quote = data.slip.advice
+      advice(id, quote);
   } 
 );
+
+// Function utilized for click listener.
+function getNewAdvice() {
+  fetch(url)
+  .then(response => response.json())
+  .then((data) => {
+      let id = data.slip.id
+      let quote = data.slip.advice
+      advice(id, quote);
+  })
+}
+
+// On dice 'click'.
+button.addEventListener('click', getNewAdvice);
+
+// Loading an alert after 3s.
+window.addEventListener('load', setTimeout(()=>{alert('Click on the dice for a new advice!')}, 3000));
